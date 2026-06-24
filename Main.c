f35734b8ad5e0c1cf9b31c740a5068e970183b58
YasiruRF <dyrfernando@gmail.com>
2026-06-24 21:43:20 +0530

    basic algo

--- Main.c
+++ Main.c
@@ -2,21 +2,26 @@
 
 #include "API.h"
 
-void log(char* text) {
+void log_message(const char* text) {
     fprintf(stderr, "%s\n", text);
     fflush(stderr);
 }
 
 int main(int argc, char* argv[]) {
-    log("Running...");
+    log_message("Running...");
     API_setColor(0, 0, 'G');
     API_setText(0, 0, "abc");
+
+    int direction[] = {API_turnLeft, API_turnRight};
+    int random_direction = rand() % 2;
+
     while (1) {
-        if (!API_wallLeft()) {
-            API_turnLeft();
+        if (!API_wallFront()) {
+            int random_choice = direction[random_direction];
+
         }
-        while (API_wallFront()) {
-            API_turnRight();
+        while (API_wallRight()) {
+            API_turnLeft();
         }
         API_moveForward();
     }
